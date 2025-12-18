c825a4082c9e617b0bddd7ba9bb86a5dc070572a
Pavel Polach <pavel.polach@tropicsquare.com>
2025-12-18 12:11:03 +0100

    feat: Add EdDSA and ECDSA methods

--- src/LibtropicArduino.h
+++ src/LibtropicArduino.h
@@ -110,6 +110,86 @@ class Tropic01 {
      */
     lt_ret_t ping(const char msgOut[], char msgIn[], const uint16_t msgLen);
 
+    /**
+     * @brief Generates ECC key in the specified ECC key slot.
+     *
+     * @param slot[in]   Slot number (TR01_ECC_SLOT_0 - TR01_ECC_SLOT_31)
+     * @param curve[in]  Type of ECC curve (TR01_CURVE_ED25519 or TR01_CURVE_P256)
+     *
+     * @retval           LT_OK Method executed successfully
+     * @retval           other Method did not execute successully, you might use lt_ret_verbose() to get verbose
+     * encoding of returned value
+     */
+    lt_ret_t eccKeyGenerate(const lt_ecc_slot_t slot, const lt_ecc_curve_type_t curve);
+
+    /**
+     * @brief Stores ECC private key to the specified ECC key slot.
+     *
+     * @param slot[in]   Slot number (TR01_ECC_SLOT_0 - TR01_ECC_SLOT_31)
+     * @param curve[in]  Type of ECC curve (TR01_CURVE_ED25519 or TR01_CURVE_P256)
+     * @param key[in]    Private key to store (32 bytes)
+     *
+     * @retval           LT_OK Method executed successfully
+     * @retval           other Method did not execute successully, you might use lt_ret_verbose() to get verbose
+     * encoding of returned value
+     */
+    lt_ret_t eccKeyStore(const lt_ecc_slot_t slot, const lt_ecc_curve_type_t curve, const uint8_t *key);
+
+    /**
+     * @brief Reads ECC public key corresponding to a private key in the specified ECC key slot.
+     *
+     * @param slot[in]          Slot number (TR01_ECC_SLOT_0 - TR01_ECC_SLOT_31)
+     * @param key[out]          Buffer for retrieving public key (32B for Ed25519, 64B for P256)
+     * @param key_max_size[in]  Size of the key buffer
+     * @param curve[out]        Type of elliptic curve public key (TR01_CURVE_ED25519 or TR01_CURVE_P256)
+     * @param origin[out]       Origin of the public key (TR01_CURVE_GENERATED or TR01_CURVE_STORED)
+     *
+     * @retval                  LT_OK Method executed successfully
+     * @retval                  other Method did not execute successully, you might use lt_ret_verbose() to get verbose
+     * encoding of returned value
+     */
+    lt_ret_t eccKeyRead(const lt_ecc_slot_t slot, uint8_t *key, const uint8_t key_max_size,
+                        lt_ecc_curve_type_t *curve, lt_ecc_key_origin_t *origin);
+
+    /**
+     * @brief Erases ECC key from the specified ECC key slot.
+     *
+     * @param slot[in]  Slot number (TR01_ECC_SLOT_0 - TR01_ECC_SLOT_31)
+     *
+     * @retval          LT_OK Method executed successfully
+     * @retval          other Method did not execute successully, you might use lt_ret_verbose() to get verbose
+     * encoding of returned value
+     */
+    lt_ret_t eccKeyErase(const lt_ecc_slot_t slot);
+
+    /**
+     * @brief Performs ECDSA signature of a message with a private ECC key stored in TROPIC01.
+     *
+     * @param slot[in]     Slot containing a private key (TR01_ECC_SLOT_0 - TR01_ECC_SLOT_31)
+     * @param msg[in]      Buffer containing a message to sign
+     * @param msg_len[in]  Length of the message
+     * @param rs[out]      Buffer for storing signature R and S bytes (must be 64 bytes)
+     *
+     * @retval             LT_OK Method executed successfully
+     * @retval             other Method did not execute successully, you might use lt_ret_verbose() to get verbose
+     * encoding of returned value
+     */
+    lt_ret_t ecdsaSign(const lt_ecc_slot_t slot, const uint8_t *msg, const uint32_t msg_len, uint8_t *rs);
+
+    /**
+     * @brief Performs EdDSA signature of a message with a private ECC key stored in TROPIC01.
+     *
+     * @param slot[in]     Slot containing a private key (TR01_ECC_SLOT_0 - TR01_ECC_SLOT_31)
+     * @param msg[in]      Buffer containing a message to sign (max length 4096 bytes)
+     * @param msg_len[in]  Length of the message
+     * @param rs[out]      Buffer for storing signature R and S bytes (must be 64 bytes)
+     *
+     * @retval             LT_OK Method executed successfully
+     * @retval             other Method did not execute successully, you might use lt_ret_verbose() to get verbose
+     * encoding of returned value
+     */
+    lt_ret_t eddsaSign(const lt_ecc_slot_t slot, const uint8_t *msg, const uint16_t msg_len, uint8_t *rs);
+
    private:
     lt_dev_arduino_t device;
     lt_ctx_mbedtls_v4_t cryptoCtx;
