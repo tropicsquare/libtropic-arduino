1e948612a125afb73ff609a6962973aadc5a82c9
Jan Medek <czmedexs@gmail.com>
2026-01-03 17:19:10 +0100

    examples: call SPI.begin, wait for Serial

--- examples/ECDSA_P256/ECDSA_P256.ino
+++ examples/ECDSA_P256/ECDSA_P256.ino
@@ -180,8 +180,13 @@ cleanup:
 // ------------------------------------------ Setup function -------------------------------------------
 void setup()
 {
+    // Initialize SPI (using the default SPI instance defined in <SPI.h>).
+    // If you want to use non-default SPI instance, don't forget to pass it to the
+    // Tropic01() constructor (otherwise it will use the default SPI instance).
+    SPI.begin();
+
     Serial.begin(9600);
-    while (!Serial);  // Wait for serial port to connect (useful for native USB)
+    while (!Serial);  // Wait for serial port to connect (useful for native USB).
 
     Serial.println("===============================================================");
     Serial.println("============ TROPIC01 ECDSA P-256 Sign & Verify ===============");
