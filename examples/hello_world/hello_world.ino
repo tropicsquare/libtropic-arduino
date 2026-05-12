065ece88aadf01c7007387f3ff2270b6f8095f8a
Jan Medek <czmedexs@gmail.com>
2026-05-12 15:14:43 +0200

    fix(hello world example): sync with libtropic 4.0.0

--- examples/hello_world/hello_world.ino
+++ examples/hello_world/hello_world.ino
@@ -52,8 +52,8 @@
 
 // Pairing Key macros for establishing a Secure Channel Session with TROPIC01.
 // Using the default Pairing Key slot 0 of Production TROPIC01 chips.
-#define PAIRING_KEY_PRIV sh0priv_prod0
-#define PAIRING_KEY_PUB sh0pub_prod0
+#define PAIRING_KEY_PRIV lt_sh0priv_prod0
+#define PAIRING_KEY_PUB lt_sh0pub_prod0
 #define PAIRING_KEY_SLOT TR01_PAIRING_KEY_SLOT_INDEX_0
 // -----------------------------------------------------------------------------------------------------
 
